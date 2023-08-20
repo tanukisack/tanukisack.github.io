2a99c33980c3ba906b160cfb376dbc0ea9a3ade1
James <61913303+tanukisack@users.noreply.github.com>
2023-08-20 12:18:01 -0400

    more changes

--- js/commands.js
+++ js/commands.js
@@ -1,10 +1,10 @@
-var youtube = "https://www.youtube.com/fknight/";
-var twitter = "https://www.twitter.com/forrestpknight/";
+var youtube = "https://www.youtube.com//";
+var twitter = "https://www.twitter.com//";
 var password = "fkcodes";
-var linkedin = "https://www.linkedin.com/in/forrestpknight/";
-var instagram = "https://www.instagram.com/forrestpknight/";
-var github = "https://github.com/forrestknight/";
-var email = 'mailto:forrest@fkcodes.com';
+var linkedin = "https://www.linkedin.com/in//";
+var instagram = "https://www.instagram.com//";
+var github = "https://github.com//";
+var email = 'mailto:.com';
 
 fwm = [
   "<br>",
